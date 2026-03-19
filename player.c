f26c2be39263baa0064e2d0896f26c34cb17b661
Vitor Serpa <vitorserpa123@gmail.com>
2026-03-19 15:08:40 -0300

    feat

--- player.c
+++ player.c
@@ -5,8 +5,14 @@
 
 #define AUDIO_CHUNK_SIZE 1024
 
+int listMusics(const char *directorypath){
+    
+}
+
 int main(int argc, char *argv[]){
     FileReader* file = malloc(sizeof(FileReader));
+
+
 
     if(file_reader_open(file, "D:\\GitHub\\AudioPlayerC\\audios\\Radiohead - Jigsaw Falling Into Place.mp3") != 0) return -1;
     
