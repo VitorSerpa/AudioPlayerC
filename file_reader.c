b0c4327a575ce9b53683ca194229e2ce5d4a9ffa
Vitor Serpa <vitorserpa123@gmail.com>
2026-03-20 12:48:34 -0300

    feat

--- file_reader.c
+++ file_reader.c
@@ -1,11 +1,11 @@
 #include "file_reader.h"
 
 int file_reader_open(FileReader *file, const char *file_path){
-    printf("%s", file_path);
+    //printf("%s", file_path);
     file->file_pointer = fopen(file_path, "rb");
     if(file->file_pointer == NULL) return -1;
 
-    printf("passou");
+    //printf("passou");
 
     fseek(file->file_pointer, 0, SEEK_END);
     file->file_size = ftell(file->file_pointer);
@@ -18,18 +18,16 @@ int file_reader_read_chunk(FileReader *file, uint8_t *buffer, size_t chunk_lengh
     return fread(buffer, 1, chunk_lenght, file->file_pointer);
 }
 
-int file_reader_metadata(FileReader *file){
+void file_reader_metadata(FileReader *file){
     fseek(file->file_pointer, 0, SEEK_SET);
     uint8_t buffer[200];
     fread(buffer, 1, 200, file->file_pointer);
 
-    for(int i = 0; i < 200; i++){
+    for(int i = 0; i < 180; i++){
         printf("%c", buffer[i]);
         if(i % 40 == 0) printf("\n");
     }
     printf("\n");
-    
-    return 0;
 }
 
 void file_reader_close(FileReader *file){
--- player.c
+++ player.c
@@ -2,21 +2,27 @@
 #include <stdint.h>
 
 #include "file_reader.h"
-
 #define AUDIO_CHUNK_SIZE 1024
 
-int listMusics(const char *directorypath){
-    
-}
+const char *directory_path = "D:\\GitHub\\AudioPlayerC\\audios\\music_list.txt";
+char buffer[50];
+
+// int listMusics(const char *directorypath){
+//     FILE* music_list_file;
+//     music_list_file = fopen(directory_path, "r");
+//     while(fgets(buffer, 100, music_list_file)) {
+//         printf("WHILE");
+//         printf("%s", buffer);
+//     }
+//     return 0;
+// }
 
 int main(int argc, char *argv[]){
-    FileReader* file = malloc(sizeof(FileReader));
-
-
-
+    FileReader* file = malloc(sizeof(FileReader));  
+    
     if(file_reader_open(file, "D:\\GitHub\\AudioPlayerC\\audios\\Radiohead - Jigsaw Falling Into Place.mp3") != 0) return -1;
     
-    file_reader_metadata(file);
+    //file_reader_metadata(file);
 
     return 0;
 }
